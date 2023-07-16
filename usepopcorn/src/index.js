df2b29d5fb41b89ca1f3e3861e0f553b90d3613e
Piotr <piotrektymula1@gmail.com>
2023-07-16 10:56:51 +0200

    tried PropTypes, implemented reusable component for stars, finished app

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { useState } from "react";
+//import './index.css';
+//import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating}/>
+      <p>This movie was rated {movieRating || "X"} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating />
+    <Test/>
   </React.StrictMode>
 );
