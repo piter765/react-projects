3647e5bcb067298368909205fafa5fd18df5a0d1
Piotr <piotrektymula1@gmail.com>
2023-07-21 09:00:47 +0200

    Added key so that component changes

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -78,6 +78,7 @@ export default function App() {
       </div>
       {selectedFriend && (
         <FormSplitBill
+          key={selectedFriend.id}
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
         />
