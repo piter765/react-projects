86d9fed2c84eda73f4e85f3b09e735fd3407361a
Piotr <piotrektymula1@gmail.com>
2023-08-18 12:39:44 +0200

    file structure, app layout, started implementing menu, order, cart

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,9 +1,50 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu, {loader as menuLoader} from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder, {action as createOrderAction} from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+import {loader as orderLoader} from "./features/order/Order";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout/>,
+    errorElement: <Error/>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error/>,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error/>,
+      },
+    ]
+  },
+]);
+
 function App() {
-  return (
-    <div>
-      Hello vite
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
